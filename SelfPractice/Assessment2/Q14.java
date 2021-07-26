88840a88a8888cfea007d31cb086512d516e0e90
AbrahamWang <wang.ibrahim222@gmail.com>
2021-07-26 07:29:14 -0700

    add new file named Assessment2

--- SelfPractice/Assessment2/Q14.java
+++ SelfPractice/Assessment2/Q14.java
@@ -4,6 +4,10 @@ public class Q14 {
     public static void main(String[] args) {
         int a = 0;
         do {
+            //  0   + 0  - 0 a=0
+            //1       1  -  1  a=1
+            //2      2   - 2  a =2
+            //3    + 3  -0   a=6
             a= a++ + --a -(a %3);
         }while (++a <4) ;
         System.out.print(a);
