1b41cf2539958d4901b8d4eb50ff7687ac4233a9
AbrahamWang <wang.ibrahim222@gmail.com>
2021-08-05 20:23:56 -0700

    Add new file

--- src/day40_CustomClass_Statics/Address/AddressObjects.java
+++ src/day40_CustomClass_Statics/Address/AddressObjects.java
@@ -3,33 +3,41 @@ package day40_CustomClass_Statics.Address;
 import java.util.Scanner;
 
 public class AddressObjects {
+
     public static void main(String[] args) {
-        Address  address= new Address();
+        Address address = new Address();
         Scanner scan = new Scanner(System.in);
 
+        System.out.println(address.country);
+        System.out.println(address.planetName); // static needs to be called through class name, not through the object
+        System.out.println(Address.country); // legal way
+        System.out.println(Address.planetName);
+
         System.out.println("Enter your building number");
-        String buildingNumber = scan.next();
+        String building = scan.next(); //7925
 
-        scan.nextLine();
-        System.out.println("Enter your Street Name:");
-        String streetName = scan.nextLine();
+        scan.nextLine(); //Enter
+        System.out.println("Enter your street name");
+        String street = scan.nextLine();
 
-        System.out.println("Enter your city Name:");
-        String cityName = scan.nextLine();
+        System.out.println("Enter your city name");
+        String city = scan.nextLine();
 
-        System.out.println("Enter your state Name:");
+        System.out.println("Enter your state name");
         String state = scan.nextLine();
 
-        System.out.println("Enter your zip code:");
-        int zipCode = scan.nextInt();
+        System.out.println("Enter your zip code");
+        int zipcode = scan.nextInt();
 
-        address.setInfo(buildingNumber,streetName,cityName,state,zipCode);
-
+        address.setInfo(building, street, city, state, zipcode);
         System.out.println(address);
 
 
 
 
 
+
     }
+
+
 }
